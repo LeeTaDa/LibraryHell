11f5606b2546e121bef477494649b9a0a2ed4f6f
LeeTaDa <160697239+LeeTaDa@users.noreply.github.com>
2024-10-03 21:06:37 +0700

    SignUp

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -8,23 +8,33 @@ function Navbar() {
         <Link href="/" className="text-lg font-bold">
           Library Website
         </Link>
-        <ul className="flex items-center space-x-4">
-          <li>
-            <Link href="/collections" className="text-gray-600 hover:text-gray-900">
-              Collections
+        <div className="flex items-center">
+          <ul className="flex items-center space-x-4 mr-6">
+            <li>
+              <Link href="/collections" className="text-gray-600 hover:text-gray-900">
+                Collections
+              </Link>
+            </li>
+            <li>
+              <Link href="/research" className="text-gray-600 hover:text-gray-900">
+                Research
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="text-gray-600 hover:text-gray-900">
+                About
+              </Link>
+            </li>
+          </ul>
+          <div className="flex items-center space-x-4">
+            <Link href="/login" className="text-gray-600 hover:text-gray-900">
+              Login
             </Link>
-          </li>
-          <li>
-            <Link href="/research" className="text-gray-600 hover:text-gray-900">
-              Research
+            <Link href="/signup" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+              Sign Up
             </Link>
-          </li>
-          <li>
-            <Link href="/about" className="text-gray-600 hover:text-gray-900">
-              About
-            </Link>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
